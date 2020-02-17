85e235add3b38ad5e070cd0b5c22d5e9e02ee3e1
Prem-K-Tamang <ktamangprem@gmail.com>
2020-02-17 23:25:06 +0545

    Task addition, edition and deletion works perfectly

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name = 'about'),
+    path('delete/<str:list_id>', views.delete, name = 'delete'),
+    path('cross_off/<str:list_id>', views.cross_off, name = "cross_off"),
+    path('uncross/<str:list_id>', views.uncross, name = 'uncross'),
+    path('edit/<str:list_id>',views.edit, name = 'edit'),
 
 ]
