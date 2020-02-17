84b03f835ffbb217308793e3b0118527aed59a0a
Prem-K-Tamang <ktamangprem@gmail.com>
2020-02-17 22:03:40 +0545

    Adding New tasks is possible with form

--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
-    context = {}
-    return render(request, 'home.html', context)
+    if request.method == "POST":
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_tasks = List.objects.all()
+            context = {'all_tasks':all_tasks}
+            messages.success(request,"New task added succesfully!")
+            return render(request, 'home.html', context)
+    else:
+        all_tasks = List.objects.all()
+        context = {'all_tasks':all_tasks}
+        return render(request, 'home.html', context)
 
 def about(request):
     context = {}
